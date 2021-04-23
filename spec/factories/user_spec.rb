7b2976b7f0dc25ed7d05ec978ae8ab02e2344206
takao888 <75712160+takao888@users.noreply.github.com>
2021-04-23 11:18:08 +0900

    テスト

---
+++ spec/factories/user_spec.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name                    {"abc"}
+    email                   {"abc@abc"}
+    password                {"abc123"}
+    password_confirmation   {"abc123"}
+    end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,63 @@
 require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+end
 
-RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    it 'nameが空では登録できない' do
-      user = User.new(name:"", email:"p@p", password:"ppp000")
-      user.valid?
+    context '上手くいく場合' do
+
+      it "全て正常に存在する" do 
+        expect(@user).to be_valid
+      end
+
+      it "nameが6文字以下" do
+        @user.name = "abcdef"
+        expect(@user).to be_valid
+      end
+
+      it "passwordが6文字以上" do
+        @user.password = "abcdef"
+        @user.password_confirmation ="abcdef"
+        expect(@user).to be_valid
+      end
     end
 
-    it 'emailが空では登録できない' do
+    context '上手くいかない場合' do
+      it 'nameが空では登録できない' do
+        @user.name
+      end
+
+      it 'emailが空では登録できない' do
+      end
+
+      it "passwordが空では登録できない" do
+      end
+
+      it "passwordとpassword_confirmationが空の時" do
+      end
+
+      it "passwordあってもでもpassword_confirmationがない時" do
+      end
+
+      it "nameが7文字以上の時" do
+      end
+      
+      it "重複したemailの時" do
+      end
+      
+      it "passwordが5文字以下の時" do
+      end
+      
+      it "" do
+      end
+      
+      it "" do
+      end
+      
+      it "" do
+      end
+      
     end
 
 
